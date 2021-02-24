821999b79f68ad485f85d26bc1390836e69bc184
ValescaGomes <77942539+ValescaGomes@users.noreply.github.com>
2021-02-24 12:06:29 -0300

    segundo commit

--- casa.py
+++ casa.py
@@ -1,20 +1,20 @@
+#Importando o pygame
 import pygame
-
+#Iniciando o jogo
 pygame.init()
-
+#largura e altura da janela de exibição
 window_width=500
 window_height=500
 
-animation_increment=10
-clock_tick_rate=20
-
+#Adicionando os valores da janela de exibição a variável tamanho.
 tamanho = (window_width, window_height)
+#a variável tela recebe e define o tamanho da janela
 tela = pygame.display.set_mode(tamanho)
-
+#Setando o nome que irá aparecer na tela do jogo
 pygame.display.set_caption("CASA")
-
+#variavel fexharTela é setada como falso
 fecharTela=False
-
+#Definindo as cores
 BRANCO=(255,255,255)
 VERMELHO=(255,0,0)
 VERDE=(0,255,0)
@@ -22,17 +22,22 @@ AZUL=(0,0,255)
 PRETO=(0,0,0)
 MARRON=(165,42,42)
 
-clock = pygame.time.Clock()
+#fundo da janela é branco
 tela.fill(BRANCO)
-
-def desenhoArvore():
+#método que desenha a casa
+def desenhoCasa():
+    #desenhar retangulo na tela, cor da casa marron e coordenadas.
     pygame.draw.rect(tela, MARRON, [110, 350, 300, 250])
     pygame.draw.polygon(tela, AZUL, [[450, 350], [275, 230], [70, 350]])
     pygame.draw.rect(tela, VERDE, [160, 400, 50, 300])
+#comando que fecha a tela se clicar no "X"
 while(fecharTela==False):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             fecharTela = True
-    desenhoArvore()
+#chamando o método desenhoCasa
+    desenhoCasa()
+#esse comando atualiza o conteúdo da tela, TIRA ESSE COMANDO E TESTA SEM ELE
     pygame.display.flip()
-    clock.tick(clock_tick_rate)
+
+
